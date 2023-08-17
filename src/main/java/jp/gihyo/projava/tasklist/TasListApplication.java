43356c02881621bef1aa73c2102350126d93e455
SS <shunsuke.sakamoto.java@gmail.com>
2023-08-17 09:48:17 +0900

    INITIAL

---
+++ src/main/java/jp/gihyo/projava/tasklist/TasListApplication.java
@@ -0,0 +1,13 @@
+package jp.gihyo.projava.tasklist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TasListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TasListApplication.class, args);
+	}
+
+}
