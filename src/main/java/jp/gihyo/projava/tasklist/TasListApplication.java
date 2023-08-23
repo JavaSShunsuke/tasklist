0de759f9c07886c23a1206bdc12d5201c906ce64
SS <shunsuke.sakamoto.java@gmail.com>
2023-08-23 15:57:02 +0900

    7.未完了絞り込み機能の追加

--- src/main/java/jp/gihyo/projava/tasklist/TasListApplication.java
+++ src/main/java/jp/gihyo/projava/tasklist/TasListApplication.java
@@ -2,8 +2,10 @@ package jp.gihyo.projava.tasklist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackageClasses = HomeController.class)
 public class TasListApplication {
 
 	public static void main(String[] args) {
